cb8c1643758daffc9896ae0508078ebebac778ea
danxdlul <37158392+danxdlul@users.noreply.github.com>
2018-11-22 10:56:32 +0100

    Delete util.js

--- beadando/student/js/util.js
+++
@@ -1,9 +0,0 @@
-
-
-function parseTemplate(template, obj) {
-	var newTemplate = template;
-	for(var key in obj) {
-		newTemplate = newTemplate.replace("$("+ key +")", obj[key]);
-	}
-	return newTemplate;
-}
